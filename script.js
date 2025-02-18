2a883558f8462fddab03b02eaa0d1a7a4e3be5a7
somethingdiff-pjt <ankit.kannaujiya.461037@gmail.com>
2025-02-18 22:43:04 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+const imgSlider = document.querySelector('.img-slider');
+const items = document.querySelectorAll('.item');
+const imgItems = document.querySelectorAll('.img-item');
+const infoItems = document.querySelectorAll('.info-item');
+
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+let colors = ['#171f2b','#3499AB','#757974','#599088','#2634AF','#8A3233']
+let indexSlider = 0;
+let index = 0;
+
+const slider = () => {
+    imgSlider.style.transform = `rotate(${indexSlider * 60}deg)`;
+
+    items.forEach(item => {
+        item.style.transform = `rotate(${indexSlider * -60}deg)`;
+    });
+
+    document.querySelector('.img-item.active').classList.remove('active');
+    imgItems[index].classList.add('active');
+
+    document.querySelector('.info-item.active').classList.remove('active');
+    infoItems[index].classList.add('active');
+
+    document.body.style.background = colors[index]
+}
+
+nextBtn.addEventListener('click', () => {
+    indexSlider++;
+    index++;
+    if(index > imgItems.length - 1)
+    {
+        index = 0;
+    }
+
+    slider();
+});
+
+prevBtn.addEventListener('click', () => {
+    indexSlider--;
+    index--;
+    if(index < 0)
+    {
+        index = imgItems.length-1;
+    }
+
+    slider();
+});
